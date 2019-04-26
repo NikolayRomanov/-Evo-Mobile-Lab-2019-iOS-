d4f428a51c2696cc66a6b0c3f198b598b92d8c84
Macbook <macbook@nikolay-macbook.local>
2019-04-26 12:09:08 +0300

    Initial Commit

---
+++ Тестовое для Evo Mobile Lab 2019 (iOS)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Тестовое для Evo Mobile Lab 2019 (iOS)
+//
+//  Created by Macbook on 26.04.2019.
+//  Copyright © 2019 Nikolay_Romanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
